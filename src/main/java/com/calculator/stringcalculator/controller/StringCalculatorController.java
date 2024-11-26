4aa51d026fcc253976d3576db7ef587b188213d8
ravindra s <ravindrakavhar1@gamil.com>
2024-11-26 12:48:15 +0530

    add project structure

---
+++ src/main/java/com/calculator/stringcalculator/controller/StringCalculatorController.java
@@ -0,0 +1,21 @@
+package com.calculator.stringcalculator.controller;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/calculator")
+public class StringCalculatorController {
+
+    private final StringCalculatorService calculatorService;
+
+    public StringCalculatorController(StringCalculatorService calculatorService) {
+        this.calculatorService = calculatorService;
+    }
+
+    @PostMapping("/add")
+    public int add(@RequestBody String numbers) {
+        return calculatorService.add(numbers);
+    }
+}
+
+
